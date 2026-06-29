c786e5c57881bc130a752bf6e7b81a578999506c
Kauan Lopes <kauanlopez@outlook.com>
2026-06-29 19:47:31 -0300

    music

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -2,11 +2,12 @@ export const globalAudio = typeof window !== 'undefined' ? new Audio('/music.mp3
 
 if (globalAudio) {
   globalAudio.loop = true;
+  globalAudio.volume = 0.8;
 }
 
 export const playMusic = () => {
   if (globalAudio) {
-    globalAudio.currentTime = 15;
+    globalAudio.currentTime = 13;
     globalAudio.play().catch(e => console.error("Audio play failed:", e));
   }
 };
